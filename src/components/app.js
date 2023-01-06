3ba933eecc40df00e017a672c6ac65dddb12d834
jovanespinosa108@gmail.com <jovanespinosa108@gmail.com>
2023-01-05 19:09:05 -0600

    created recent-posts and search bar components and imported to app file

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+
 import Logo from "./logo";
+import SearchBar from './search-bar';
+import RecentPosts from './recent-posts';
 
 export default class App extends Component {
   render() {
@@ -7,6 +10,8 @@ export default class App extends Component {
       <div className='app'>
         <div>
           <Logo />
+          <SearchBar />
+          <RecentPosts />
         </div>
       </div>
     );
