3661f5ff0bf66a5944a2c9652456ad672b79549e
jovanespinosa108@gmail.com <jovanespinosa108@gmail.com>
2023-01-18 20:36:08 -0600

    add SET_RESULTS_POSTS to reducer

--- src/components/results.js
+++ src/components/results.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Logo from "./logo";
 import SearchBar from "./search-bar";
-import RecentPosts from "./recent-posts";
+import ResultsPosts from "./results-posts";
 
 import { connect } from 'react-redux';
 import * as actions from '../actions';
@@ -16,7 +16,7 @@ class Results extends Component {
             <div>
                 <Logo size={55} />
                 <SearchBar onSubmit={(query) => this.handleSearchBarSubmit(query)} />
-                <RecentPosts />
+                <ResultsPosts />
             </div>
         )
     }
