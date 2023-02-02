3b7e7e441906d1c4aeaf05e932fdfb2c5cd448ac
jovanespinosa108@gmail.com <jovanespinosa108@gmail.com>
2023-02-01 19:00:22 -0600

    added no-content post links message when no post exist yet, and applied flex styles to post links

--- src/components/post.js
+++ src/components/post.js
@@ -49,7 +49,10 @@ class Post extends Component {
                     </div>
                 </div>
             )
-        });
+        })
+        if (links == 0) {
+            return <div className="no-content">No Post Links</div>
+        }
         return links;
     }
 
